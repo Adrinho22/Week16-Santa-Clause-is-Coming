380658072e72c88a430fbed1f9ef6e42f9ede9f6
Opilane TTHK <opilane@tthk.ee>
2021-12-14 11:11:14 +0200

    asds

---
+++ asds/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace asds
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.house = new System.Windows.Forms.PictureBox();
+            this.moon = new System.Windows.Forms.PictureBox();
+            this.santa = new System.Windows.Forms.PictureBox();
+            this.snowflake = new System.Windows.Forms.PictureBox();
+            this.tree = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.playAgain = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.house)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.santa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snowflake)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.Transparent;
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(1, 364);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(799, 86);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            this.ground.Click += new System.EventHandler(this.ground_Click);
+            // 
+            // house
+            // 
+            this.house.Image = ((System.Drawing.Image)(resources.GetObject("house.Image")));
+            this.house.Location = new System.Drawing.Point(60, 285);
+            this.house.Name = "house";
+            this.house.Size = new System.Drawing.Size(110, 104);
+            this.house.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.house.TabIndex = 1;
+            this.house.TabStop = false;
+            this.house.Click += new System.EventHandler(this.house_Click);
+            // 
+            // moon
+            // 
+            this.moon.Image = ((System.Drawing.Image)(resources.GetObject("moon.Image")));
+            this.moon.Location = new System.Drawing.Point(1, 1);
+            this.moon.Name = "moon";
+            this.moon.Size = new System.Drawing.Size(107, 81);
+            this.moon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.moon.TabIndex = 2;
+            this.moon.TabStop = false;
+            this.moon.Click += new System.EventHandler(this.moon_Click);
+            // 
+            // santa
+            // 
+            this.santa.Image = ((System.Drawing.Image)(resources.GetObject("santa.Image")));
+            this.santa.Location = new System.Drawing.Point(60, 133);
+            this.santa.Name = "santa";
+            this.santa.Size = new System.Drawing.Size(165, 95);
+            this.santa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.santa.TabIndex = 3;
+            this.santa.TabStop = false;
+            this.santa.Click += new System.EventHandler(this.santa_Click);
+            // 
+            // snowflake
+            // 
+            this.snowflake.Image = ((System.Drawing.Image)(resources.GetObject("snowflake.Image")));
+            this.snowflake.Location = new System.Drawing.Point(597, 1);
+            this.snowflake.Name = "snowflake";
+            this.snowflake.Size = new System.Drawing.Size(100, 97);
+            this.snowflake.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.snowflake.TabIndex = 4;
+            this.snowflake.TabStop = false;
+            this.snowflake.Click += new System.EventHandler(this.snowflake_Click);
+            // 
+            // tree
+            // 
+            this.tree.BackColor = System.Drawing.Color.Transparent;
+            this.tree.Image = ((System.Drawing.Image)(resources.GetObject("tree.Image")));
+            this.tree.Location = new System.Drawing.Point(639, 274);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(100, 155);
+            this.tree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tree.TabIndex = 5;
+            this.tree.TabStop = false;
+            this.tree.Click += new System.EventHandler(this.tree_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(132, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Score: 0";
+            this.label1.Click += new System.EventHandler(this.timer1_Tick);
+            // 
+            // playAgain
+            // 
+            this.playAgain.AutoSize = true;
+            this.playAgain.BackColor = System.Drawing.Color.Red;
+            this.playAgain.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.playAgain.Location = new System.Drawing.Point(304, 191);
+            this.playAgain.Name = "playAgain";
+            this.playAgain.Size = new System.Drawing.Size(146, 37);
+            this.playAgain.TabIndex = 7;
+            this.playAgain.Text = "Play Again";
+            this.playAgain.Visible = false;
+            this.playAgain.Click += new System.EventHandler(this.playAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playAgain);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tree);
+            this.Controls.Add(this.snowflake);
+            this.Controls.Add(this.santa);
+            this.Controls.Add(this.moon);
+            this.Controls.Add(this.house);
+            this.Controls.Add(this.ground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Keydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.house)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.santa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snowflake)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox house;
+        private System.Windows.Forms.PictureBox moon;
+        private System.Windows.Forms.PictureBox santa;
+        private System.Windows.Forms.PictureBox snowflake;
+        private System.Windows.Forms.PictureBox tree;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label playAgain;
+    }
+}
+
---
+++ asds/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asds
+{
+    public partial class Form1 : Form
+    {
+        int gravity = 10;
+        int pipeSpeed = 6;
+        int score = 0;
+        int backroundSpeed = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void santa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void snowflake_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void house_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ground_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            santa.Top += gravity;
+            snowflake.Left -= pipeSpeed;
+            tree.Left -= pipeSpeed;
+            moon.Left -= backroundSpeed;
+            house.Left -= backroundSpeed;
+            label1.Text = $"score: {score}";
+
+            if (snowflake.Left < -100)
+            {
+                snowflake.Left = 1000;
+            }
+            if (tree.Left < -100)
+            {
+                tree.Left = 900;
+                score++;
+            }
+            if (moon.Left < -100)
+            {
+                moon.Left = 800;
+            }
+            if (house.Left < -100)
+            {
+                house.Left = 900;
+            }
+            if (santa.Top < -25)
+            {
+                gameOver();
+            }
+            if (santa.Bounds.IntersectsWith(snowflake.Bounds) || santa.Bounds.IntersectsWith(tree.Bounds) || santa.Bounds.IntersectsWith(ground.Bounds))
+            {
+                gameOver();
+            }
+        }
+
+        private void Keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -5;
+            }
+        }
+
+        private void Keyup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5;
+            }
+        }
+        private void gameOver()
+        {
+            timer1.Stop();
+            label1.Text = $"Game Over";
+            playAgain.Visible = true;
+        }
+
+        private void playAgain_Click(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+    }
+    
+}
